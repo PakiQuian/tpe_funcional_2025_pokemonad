36e6bbc5246baa724ec4f25b08e912f8a7f4f7d2
PakiQuian <f.quianblanco@gmail.com>
2026-02-13 14:51:32 -0300

    Arreglo para barra de vida en batalla

--- game-client/app/screens/BattleScreen.hs
+++ game-client/app/screens/BattleScreen.hs
@@ -51,47 +51,42 @@ drawEnemyUnit bp spriteMap =
       [ case Map.lookup (pId (bpOriginal bp)) spriteMap of
           Just pic -> scale 2.5 2.5 pic
           Nothing -> scale 0.2 0.2 $ text "?",
-        translate (-300) 100 $ drawHUD bp False
+        translate (-230) 80 $ drawHUD bp
       ]
 
 drawPlayerUnit :: BattlePokemon -> Map.Map Int Picture -> Picture
 drawPlayerUnit bp spriteMap =
   translate (-210) (-105) $
     pictures
-      [ -- Sprite del Jugador (Back Sprite)
-        case Map.lookup (pId (bpOriginal bp)) spriteMap of
+      [ case Map.lookup (pId (bpOriginal bp)) spriteMap of
           Just pic -> scale 4.5 4.5 pic
           Nothing -> scale 0.2 0.2 $ text "?",
-        -- HUD Jugador - Un poco a la derecha
-        translate 300 50 $ drawHUD bp True
+        translate (-230) 80 $ drawHUD bp
       ]
 
 -- ===============================================================
 -- HUD (Head Up Display - Barras de Vida)
 -- ===============================================================
 
-drawHUD :: BattlePokemon -> Bool -> Picture
-drawHUD bp isPlayer =
+drawHUD :: BattlePokemon -> Picture
+drawHUD bp =
   pictures
     [ -- Caja Blanca semitransparente
-      color (makeColorI 255 255 255 200) $ rectangleSolid 260 80,
-      color black $ rectangleWire 260 80,
+      color (makeColorI 255 255 255 200) $ rectangleSolid 265 65,
+      color black $ rectangleWire 265 65,
       -- Nombre
-      translate (-120) 15 $ scale 0.15 0.15 $ color black $ text (pName (bpOriginal bp)),
+      translate (-120) 8 $ scale 0.15 0.15 $ color black $ text (pName (bpOriginal bp)),
       -- Barra de Vida (Fondo Gris)
-      translate (-80) (-15) $ color (makeColorI 100 100 100 255) $ rectangleSolid 200 10,
+      translate (-35) (-13) $ color (makeColorI 100 100 100 255) $ rectangleSolid 170 10,
       -- Barra de Vida (Actual - Verde/Amarillo/Rojo)
       let pct = fromIntegral (bpHp bp) / fromIntegral (bpMaxHp bp)
           barColor
             | pct > 0.5 = makeColorI 50 200 50 255 -- Verde
             | pct > 0.2 = makeColorI 200 200 50 255 -- Amarillo
             | otherwise = makeColorI 200 50 50 255 -- Rojo
-          width = 200 * pct
-       in translate (-80 - (200 - width) / 2) (-15) $ color barColor $ rectangleSolid width 10,
-      -- Texto HP (Solo para el jugador)
-      if isPlayer
-        then translate 40 (-15) $ scale 0.12 0.12 $ color black $ text (show (bpHp bp) ++ "/" ++ show (bpMaxHp bp))
-        else blank
+          width = 170 * pct
+       in translate (-35 - (170 - width) / 2) (-13) $ color barColor $ rectangleSolid width 10,
+      translate 60 (-18) $ scale 0.12 0.12 $ color black $ text (show (bpHp bp) ++ "/" ++ show (bpMaxHp bp))
     ]
 
 -- ===============================================================
